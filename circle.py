6cc80d6404acc4cefa93245983ea8fec821d6c78
Omkar Yadav <55269334+omkar-yadav-12@users.noreply.github.com>
2021-11-17 13:08:57 -0500

    Update circle.py

--- circle.py
+++ circle.py
@@ -21,20 +21,26 @@ img = cv2.medianBlur(img, 5)
 
 # finds the circles in the grayscale image using the Hough transform
 circles = cv2.HoughCircles(image=img, method=cv2.HOUGH_GRADIENT, dp=0.9, 
-                            minDist=80, param1=110, param2=39, maxRadius=70)
+                            minDist=80, param1=110, param2=39, minRadius=0, maxRadius=70)
 
-for co, i in enumerate(circles[0, :], start=1):
-    # # draw the outer circle in green
-    # cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # # draw the center of the circle in red
-    # cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+if circles is not None:
 
-    pass
-    
-# print the number of circles detected
-print("Number of circles detected:", co)
-# save the image, convert to BGR to save with proper colors
-# cv2.imwrite("coins_circles_detected.png", cimg)
-# show the image
-plt.imshow(cimg)
-plt.show()
+    for co, i in enumerate(circles[0, :], start=1):
+        # # draw the outer circle in green
+        cv2.circle(cimg,(i[0],i[1]),int(i[2]),(0,255,0),2)
+        # # draw the center of the circle in red
+        # cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+        pass
+        
+    # print the number of circles detected
+    print("Number of circles detected:", co)
+    # save the image, convert to BGR to save with proper colors
+    cv2.imwrite("coins_circles_detected.png", cimg)
+    # show the image
+    plt.imshow(cimg)
+    plt.show()
+
+else:
+
+    print("No circles detected")
